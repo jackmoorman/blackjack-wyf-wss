cf91985387ebc7b61953c8dbd68fdca0c4ae55ed
johnmoormaniii <johnmoormaniii@gmail.com>
2023-05-20 17:47:16 -0400

    Able to connect and join rooms, leaving the room is not finished

--- server.js
+++ server.js
@@ -1,21 +1,79 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-  }
-});
+const cors = require('cors');
+const WebSocket = require('ws');
+const server = require('http').createServer(app);
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT;
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-})
+app.use(cors());
 
-server.listen(PORT, () => console.log(`Socket.io server running on PORT: ${PORT}`));
+const wss = new WebSocket.Server({ server });
 
-// server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+const rooms = new Map();
+
+wss.on('connection', (ws) => {
+  console.log('A user connected.');
+
+  ws.on('message', (data, isBinary) => {
+    console.log('MESSAGE: ', JSON.parse(data));
+    const { room, user, method } = JSON.parse(data);
+    switch (method) {
+      case 'join':
+        if (!rooms.has(room)) {
+          rooms.set(room, {
+            inProgress: false,
+            users: [
+              {
+                id: user.id,
+                name: user.username,
+                isHost: true,
+                ws: ws,
+              },
+            ],
+          });
+        } else {
+          const currentRoom = rooms.get(room);
+          currentRoom.users.push({
+            id: user.id,
+            name: user.username,
+            isHost: false,
+            ws: ws,
+          });
+          rooms.set(room, currentRoom);
+        }
+        console.log('ROOMS: ', rooms);
+        const currentUsers = rooms.get(room);
+        for (let i = 0; i < currentUsers.users.length; i++) {
+          console.log('SENDING TO: ', currentUsers.users[i].name);
+          currentUsers.users[i].ws.send(
+            JSON.stringify({
+              type: 'connection',
+              message: `${user.username} has joined the session.`,
+            }),
+            { binary: isBinary }
+          );
+        }
+        break;
+
+      case 'leave':
+        const currentRoom = rooms.get(room);
+        currentRoom.users = currentRoom.users.filter((user) => user.ws !== ws);
+        console.log('USERS: ', currentRoom.users.length);
+        if (currentRoom.users.length === 0) {
+          rooms.delete(room);
+          console.log('The last remaining user left the session.');
+          console.log('ROOMS: ', rooms);
+          break;
+        }
+        rooms.set(room, currentRoom);
+        console.log('A user left the session.');
+        break;
+    }
+  });
+});
+
+server.listen(PORT, () =>
+  console.log(`WebSocket server running on PORT: ${PORT}`)
+);
