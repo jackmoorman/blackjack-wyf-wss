ebf0a7e8d493c8195ee5ebbd77afd0328569c498
johnmoormaniii <johnmoormaniii@gmail.com>
2023-05-20 15:00:19 -0400

    Committing before switching to WS library

--- server.js
+++ server.js
@@ -2,5 +2,20 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  }
+});
+require('dotenv').config();
 
-server.listen(3100, () => console.log(`Server running on port 3100`))
+const PORT = process.env.PORT || 3500;
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+})
+
+server.listen(PORT, () => console.log(`Socket.io server running on PORT: ${PORT}`));
+
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
